557caa8697afa71184628b4fdcb253c79bb1d44f
orlov_anton <orlov_anton@mail.ru>
2023-10-07 13:24:57 +0300

    Initial commit1

---
+++ src/main/java/com/minibank/mbank_users/MBankUsersApplication.java
@@ -0,0 +1,13 @@
+package com.minibank.mbank_users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MBankUsersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MBankUsersApplication.class, args);
+    }
+
+}
